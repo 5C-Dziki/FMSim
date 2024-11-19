2884fb3295543a305663b11fbac5f4a1064891c7
Maksymilian Fikus <maksymilian.fikus@icloud.com>
2024-11-19 13:01:06 +0100

    Remove Formation from FootballClub.cs

--- FMSim/FMBase/Clubs/FootballClub.cs
+++ FMSim/FMBase/Clubs/FootballClub.cs
@@ -1,7 +1,6 @@
 namespace FMBase.Clubs;
 
-public class FootballClub(string name, string description, string colors, int[] members, string formation, string jersey) : Club(name, description, colors, members)
+public class FootballClub(string name, string description, string colors, int[] members, string jersey) : Club(name, description, colors, members)
 {
-    public string Formation = formation;
     public string Jersey = jersey;
 }
