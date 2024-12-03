638f2050d5334bc731cf8996ffadff1ba7b51cb8
Dominik <dominik.konieczny@uczen.zsk.poznan.pl>
2024-12-03 12:19:30 +0100

    Configure JSON dump and Base class

--- FMSim/FMBase/Base.cs
+++ FMSim/FMBase/Base.cs
@@ -1,35 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FMBase.Hoomans;
-using FMInterface;
-
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace FMBase
 {
-    class Base
+    public class Base
     {
-        string userName;
-        string saveName;
-        DateTime createdAt;
-
-        async void jsonDump()
+        async static public void JsonDump(Object obj, string name)
         {
-            await using FileStream createStream = File.Create(saveName + ".json");
-            await JsonSerializer.SerializeAsync(createStream, this);
-        }
-
-        public void Main(string[] args)
-        {
-            Base cos = new Base();
-            cos.userName = "mish";
-            cos.saveName = "test";
-            cos.createdAt = DateTime.Now;
-
-            cos.jsonDump();
+            await using FileStream createStream = File.Create(name + ".json");
+            await JsonSerializer.SerializeAsync(createStream, obj);
         }
     }
 }
