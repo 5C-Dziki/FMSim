9f973e7b5dda352a4808e840a5d0f6c1f7c0a88c
Jan Grzeskowiak <jan.grzeskowiak@uczen.zsk.poznan.pl>
2024-12-17 12:34:00 +0100

    Add multiple players functionality to Match

--- FMSim/FMBase/Clubs/FootballClub.cs
+++ FMSim/FMBase/Clubs/FootballClub.cs
@@ -1,9 +1,21 @@
 using FMBase.Hoomans;
-
+using FMBase.ClubsTemp;
 namespace FMBase.Clubs;
 
 public class FootballClub(string name, string description, string colors, Player[] members, string jersey) : Club(name, description, colors)
 {
     public string Jersey = jersey;
     public Player[] Members = members;
+
+    public FootballClub LoadClub(bool isPlayer)
+    {
+        if (isPlayer)
+        {
+            return FootballClubsTemp.Generate(true);
+        }
+        else
+        {
+            return FootballClubsTemp.Generate(false);
+        }
+    }
 }
