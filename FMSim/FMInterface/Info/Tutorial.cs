6a2b6e8ee157487acff3861f6bdba508e661590c
kejmil <kamil.checinski@uczen.zsk.poznan.pl>
2024-12-17 12:52:34 +0100

    third naming issue

--- FMSim/FMInterface/Info/Tutorial.cs
+++ FMSim/FMInterface/Info/Tutorial.cs
@@ -45,7 +45,7 @@
                     new[]
                     {
                         "In the 'Start Game' tab, press 'New Game' to start a new game.",
-                        "You will have two options: Football Club and User Club.",
+                        "You will have two options: Football Club and Fan Club.",
                         "1. Choose your club type (Football Club or Fan Club).",
                         "2. Fill out the form to create YOUR OWN CLUB!",
                         "3. Play and enjoy the game!",
